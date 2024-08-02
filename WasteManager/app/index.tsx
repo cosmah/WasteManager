0ff05c2e4681c0a9ad32a540460ac45cc66a509f
cosmah <cosmahke4@gmail.com>
2024-08-02 13:39:29 +0300

    working on the landing page

--- WasteManager/app/index.tsx
+++ WasteManager/app/index.tsx
@@ -1,18 +1,27 @@
-import { Text, View } from "react-native";
-import { StatusBar } from "expo-status-bar";
-import { Link } from 'expo-router';
 import React from "react";
+import { Text, View, Image } from "react-native";
 import { styled } from 'nativewind';
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ScrollView } from 'react-native';
+
+import landingImage from '../assets/images/landing.jpeg'; // Correct path to the image file
 
 const StyledView = styled(View);
-const StyledText = styled(Text);
+const SImage = styled(Image);
+const StyledSafeAreaView = styled(SafeAreaView);
 
 export default function App() {
   return (
-    <StyledView className="flex-1 items-center justify-center bg-white">
-      <StyledText className="text-3xl font-pblack">Yo Waste Manager</StyledText>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{ color: 'blue' }}>Home</Link>
-    </StyledView>
+    <StyledSafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{height: '100%'}}>
+        <StyledView className="w-full justify-center items-center h-full px-4">
+            <SImage
+              source={landingImage}
+              className="w-[130px] h-[84px]" 
+              resizeMode="contain"
+            />
+        </StyledView>
+      </ScrollView>
+    </StyledSafeAreaView>
   );
 }
