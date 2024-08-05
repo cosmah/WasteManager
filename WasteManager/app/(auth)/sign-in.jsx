2f8ef1f6709024a24ef0b433df08ac04c676842b
cosmah <cosmahke4@gmail.com>
2024-08-05 15:09:39 +0300

    tryng to export the sign-in button

--- WasteManager/app/(auth)/sign-in.jsx
+++ WasteManager/app/(auth)/sign-in.jsx
@@ -1,15 +1,16 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { View, Text, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { styled } from "nativewind";
 import { images } from "../../assets/images";
 import { Image } from "react-native";
-const StyledText = styled(Text);
+import FormField from "@/components/FormField";
+import CustomButtons from "@/components/CustomButtons";
 
+const StyledText = styled(Text);
 const SafeAreaViewContainer = styled(SafeAreaView);
 const SImage = styled(Image);
 const StyledView = styled(View);
-import FormField from "@/components/FormField";
 
 const SignIn = () => {
   //create user state field
@@ -17,6 +18,16 @@ const SignIn = () => {
     email: "",
     password: "",
   });
+
+  //loading state
+  const [isSubmitting, setIsSubmitting] = useState(false);
+
+  //LOGIN FUNCTION
+  const submit = () => {
+    setIsSubmitting(true);
+    // Add your login logic here
+    setIsSubmitting(false);
+  };
 
   return (
     <SafeAreaViewContainer className="bg-primary h-full">
@@ -29,25 +40,30 @@ const SignIn = () => {
           />
 
           <StyledText className="text-2xl text-white text-semibold mt-10 fon-psemibold">
-            {" "}
             Log in to YoWaste Manager
           </StyledText>
 
           <FormField 
-          title="Email Address" 
-          value={form.email} 
-          handlerChangeText={(e) => setForm({...form, email: e})}
-          otherStyles="mt-7"
-          keyboardType="email-address"
+            title="Email Address" 
+            value={form.email} 
+            handlerChangeText={(e) => setForm({...form, email: e})}
+            otherStyles="mt-7"
+            keyboardType="email-address"
           />
           <FormField 
-          title="Password" 
-          value={form.password} 
-          handlerChangeText={(e) => setForm({...form, password: e})}
-          otherStyles="mt-7"
-          keyboardType="password"
+            title="Password" 
+            value={form.password} 
+            handlerChangeText={(e) => setForm({...form, password: e})}
+            otherStyles="mt-7"
+            keyboardType="password"
           />
         </StyledView>
+        <CustomButtons
+          title="Sign In"
+          handlePress={submit}
+          containerStyle="mt-7"
+          isLoading={isSubmitting}
+        />
       </ScrollView>
     </SafeAreaViewContainer>
   );
--- WasteManager/components/FormField.jsx
+++ WasteManager/components/FormField.jsx
@@ -1,10 +1,15 @@
-import { Text, TextInput, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { styled } from "nativewind";
 import { useState } from "react";
+import { Image } from "react-native";
+
+import icons from "@/constants/icons";
 
 const StyledView = styled(View);
 const StyledText = styled(Text);
 const STextInput = styled(TextInput);
+
+const ImageComponent = styled(Image);
 
 const FormField = ({
   title,
@@ -24,7 +29,7 @@ const FormField = ({
       </StyledText>
 
       <StyledView className="border-2 border-black-500 w-full h-16 px-4 
-      bg-black-100 rounded-2xl focus:border-secondary items-center">
+      bg-black-100 rounded-2xl focus:border-secondary items-center flex-row">
 
         <STextInput 
             className="flex-1 text-white font-psemibold text-base"
@@ -34,6 +39,14 @@ const FormField = ({
             onChange={handleChangeText}
             secureTextEntry={title === "Password" && !ShowPassword}
         />
+
+        {title === "Password" && (
+            <TouchableOpacity onPress={() => setShowPassword(!ShowPassword)}>
+                <ImageComponent source={!ShowPassword ? icons.eye : icons.eyeHide} 
+                className="w-6 h-6" resizeMode="contain"/>
+            </TouchableOpacity>
+        )}
+
       </StyledView>
     </StyledView>
   );
