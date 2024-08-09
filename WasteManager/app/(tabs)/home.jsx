da9b1d84aac07b78ee9039bb171ebc001110a510
cosmah <cosmahke4@gmail.com>
2024-08-09 22:42:15 +0300

    working on the font

--- WasteManager/app/(tabs)/home.jsx
+++ WasteManager/app/(tabs)/home.jsx
@@ -10,6 +10,7 @@ import React, { useEffect, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { styled } from "nativewind";
 import { getCurrentUser } from "@/lib/appwrite";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import * as Location from "expo-location";
 
 const StyledView = styled(View);
@@ -55,25 +56,80 @@ const Home = () => {
   }
 
   return (
-    <StyledSafeAreaView className="bg-primary h-full ">
+    <StyledSafeAreaView className="bg-primary h-full">
       <StyledView style={{ flex: 1 }}>
-  <StyledView className="flex-row justify-between items-center mb-6 p-5">
-    <StyledView>
-      <StyledText className="text-2xl font-psemibold text-secondary">
-        Hi, {username ? `${username}` : "Loading..."}
-      </StyledText>
-      <StyledText className="text-sm text-gray-100">
-        {locationText}
-      </StyledText>
-    </StyledView>
-  </StyledView>
+        <StyledView className="flex-row justify-between items-center mb-6 p-5">
+          <StyledView>
+            <StyledText className="text-2xl font-psemibold text-secondary">
+              Hi, {username ? `${username}` : "Loading..."}
+            </StyledText>
+            <StyledText className="text-sm text-gray-100">
+              {locationText}
+            </StyledText>
+          </StyledView>
+        </StyledView>
 
-  <StyledView style={{ backgroundColor: "#7A7777", flex: 1 }} className="rounded-xl">
-    <StyledText className="p-5 text-2xl font-psemibold">Quick statistics</StyledText>
-  </StyledView>
-</StyledView>
+        <StyledView
+          style={{ backgroundColor: "#7A7777", flex: 1 }}
+          className="rounded-xl"
+        >
+          <StyledText className="p-5 text-2xl font-psemibold">
+            Quick statistics
+          </StyledText>
+          <StyledView className="flex-row justify-around mt-5">
+            <StyledView className="items-center">
+              <StyledView style={styles.iconBackground}>
+                <StyledText className="text-4xl text-lime-500">1500 kg</StyledText>
+              </StyledView>
+              <StyledText className="text-black font-psemibold mt-2">
+                Total Waste Collected
+              </StyledText>
+            </StyledView>
+            <StyledView className="items-center">
+              <StyledView style={styles.iconBackground}>
+                <StyledText className="text-4xl text-lime-500">1200 kg</StyledText>
+              </StyledView>
+              <StyledText className="text-black font-psemibold mt-2">
+                Total Waste Recycled
+              </StyledText>
+            </StyledView>
+          </StyledView>
+          <StyledView className="flex-row justify-around mt-5">
+            <StyledView className="items-center">
+              <StyledView style={styles.iconBackground}>
+                <StyledText className="text-4xl text-lime-500">800 kg</StyledText>
+              </StyledView>
+              <StyledText className="text-black font-psemibold mt-2">Organic Waste</StyledText>
+            </StyledView>
+            <StyledView className="items-center">
+              <StyledView style={styles.iconBackground}>
+                <StyledText className="text-4xl text-lime-500">700 kg</StyledText>
+              </StyledView>
+              <StyledText className="text-black font-psemibold mt-2">
+                Synthetic Waste
+              </StyledText>
+            </StyledView>
+          </StyledView>
+        </StyledView>
+      </StyledView>
     </StyledSafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  iconBackground: {
+    backgroundColor: '#000',
+    borderRadius: 150,
+    padding: 20,
+    marginTop:50,
+    elevation: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default Home;
