9498f6cdd531f34250a5516752bd8885cafb3490
cosmah <cosmahke4@gmail.com>
2024-08-10 21:51:57 +0300

    rendered the name and image up

--- WasteManager/app/(tabs)/profile.jsx
+++ WasteManager/app/(tabs)/profile.jsx
@@ -1,13 +1,13 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
-import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Image } from "react-native";
+import React, { useEffect, useState } from "react";
 import { getCurrentUser } from "@/lib/appwrite"; // Assuming appwrite.js is in the same directory
-import { styled } from 'nativewind';
-import { SafeAreaView } from 'react-native-safe-area-context';
-
+import { styled } from "nativewind";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const StyledSafeAreaView = styled(SafeAreaView);
 const StyledView = styled(View);
 const StyledText = styled(Text);
+const StyledImage = styled(Image);
 
 const Profile = () => {
   const [user, setUser] = useState(null);
@@ -30,11 +30,27 @@ const Profile = () => {
   }
 
   return (
-    <StyledSafeAreaView className='bg-primary h-full' style={styles.container}>
-      <Image source={{ uri: user.avatar }} style={styles.avatar} />
-      <StyledText className="text-2xl font-psemibold text-secondary" style={styles.name}>{user.username}</StyledText>
-      <StyledText className="text-2xl font-psemibold text-secondary-100" style={styles.email}>{user.email}</StyledText>
-    </StyledSafeAreaView >
+    <StyledSafeAreaView className="bg-primary h-full" style={styles.container}>
+      <StyledView style={{ flex: 1 }}>
+        <StyledView className="flex-row items-center mb-6 p-5" style={styles.profileContainer}>
+          <StyledImage className="" source={{ uri: user.avatar }} style={styles.avatar} />
+          <StyledView style={styles.infoContainer}>
+            <StyledText
+              className="text-2xl font-psemibold text-secondary"
+              style={styles.name}
+            >
+              {user.username}
+            </StyledText>
+            <StyledText
+              className="text-2xl font-psemibold text-secondary-100"
+              style={styles.email}
+            >
+              {user.email}
+            </StyledText>
+          </StyledView>
+        </StyledView>
+      </StyledView>
+    </StyledSafeAreaView>
   );
 };
 
@@ -43,23 +59,27 @@ export default Profile;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     padding: 20,
   },
+  profileContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
   avatar: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
-    marginBottom: 20,
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    marginRight: 20,
+  },
+  infoContainer: {
+    flex: 1,
   },
   name: {
-    fontSize: 24,
-    fontWeight: 'bold',
+    fontSize: 20,
+    fontWeight: "bold",
     marginBottom: 10,
   },
   email: {
-    fontSize: 18,
-
+    fontSize: 15,
   },
 });
