190462ff429ec20ef5caa851e487f29fe38dd3a4
cosmah <cosmahke4@gmail.com>
2024-08-13 13:13:11 +0300

    implemented the theme

--- WasteManager/app/views/bookingDetails.jsx
+++ WasteManager/app/views/bookingDetails.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
 import { useRoute } from "@react-navigation/native";
 import { styled } from "nativewind";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { getCurrentUser, fetchBookings } from "@/lib/appwrite"; 
+import { getCurrentUser, fetchBookings } from "@/lib/appwrite";
 
 const StyledSafeAreaView = styled(SafeAreaView);
 const StyledView = styled(View);
@@ -19,15 +26,24 @@ const BookingDetails = () => {
 
   return (
     <StyledSafeAreaView className="bg-primary h-full" style={styles.container}>
-      <View style={styles.container}>
-      <Text style={styles.title}>Booking Details</Text>
-      <StyledText style={styles.bookingText}>Service Type: {booking.serviceType}</StyledText>
-                  <StyledText style={styles.bookingText}>Date: {new Date(booking.pickupDate).toLocaleDateString()}</StyledText>
-                  <StyledText style={styles.bookingText}>Time: {new Date(booking.pickupTime).toLocaleTimeString()}</StyledText>
-      {/* Add more details as necessary */}
-    </View>
-    </StyledSafeAreaView>
-    
+  {/* Header View */}
+  <StyledView style={styles.header}>
+    <Text style={styles.title}>Booking Details</Text>
+  </StyledView>
+  
+  {/* Content View */}
+  <StyledView style={styles.content} className="rounded-xl">
+    <StyledText style={styles.bookingText}>
+      Service Type: {booking.serviceType}
+    </StyledText>
+    <StyledText style={styles.bookingText}>
+      Date: {new Date(booking.pickupDate).toLocaleDateString()}
+    </StyledText>
+    <StyledText style={styles.bookingText}>
+      Time: {new Date(booking.pickupTime).toLocaleTimeString()}
+    </StyledText>
+  </StyledView>
+</StyledSafeAreaView>
   );
 };
 
@@ -36,15 +52,23 @@ export default BookingDetails;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  header: {
+    flex: 0.1, // Adjust this value as needed
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  content: {
+    flex: 0.9, // Adjust this value as needed
+    backgroundColor: "#7A7777",
     padding: 20,
   },
   title: {
     fontSize: 24,
-    fontWeight: "bold",
-    marginBottom: 20,
+    fontWeight: 'bold',
   },
-  detailText: {
+  bookingText: {
     fontSize: 18,
-    marginBottom: 10,
+    marginVertical: 5,
   },
 });
