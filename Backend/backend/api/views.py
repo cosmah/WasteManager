3b12e367dd82e5bb203921e66d06790568136dbf
cosmah <cosmahke4@gmail.com>
2024-08-27 21:29:53 +0300

    working on booking andpoint

--- Backend/backend/api/views.py
+++ Backend/backend/api/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from rest_framework import generics
+from rest_framework import generics, status, viewsets
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
-from rest_framework.views import APIView  # Add this import
+from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
-from .serializers import UserSerializer
-from .serializers import BookingSerializer
+from .serializers import UserSerializer, BookingSerializer
 from .models import Booking
-from rest_framework import viewsets
-from .models import Booking
-
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     permission_classes = (AllowAny,)
@@ -31,16 +27,13 @@ class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
     permission_classes = [AllowAny]
 
-
-
-
 class BookingViewSet(viewsets.ModelViewSet):
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
-    
+
     def get_permissions(self):
         if self.action in ['create', 'list']:
-            return []
+            return [IsAuthenticated()]
         return super().get_permissions()
 
     def get_serializer(self, *args, **kwargs):
@@ -51,7 +44,7 @@ class BookingViewSet(viewsets.ModelViewSet):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        self.perform_create(serializer)
+        serializer.save(user=request.user)  # Associate the booking with the authenticated user
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
